8a7c3d71955a0013bab22c1da2564bfc61147907
DEVA266 <devats266@gmail.com>
2025-07-04 22:15:28 +0530

    4-7-25

--- sample/src/rps.java
+++ sample/src/rps.java
@@ -3,7 +3,6 @@ import java.util.Random;
 
 public class rps {
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         boolean isRunning = true;
